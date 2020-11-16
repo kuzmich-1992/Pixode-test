46927a7b8e386f154ded8c3d8eb9b8e2b826605e
Dima <kuzmichman92@gmail.com>
2020-11-16 20:49:15 +0200

    many-through

--- app/models/level.rb
+++ app/models/level.rb
@@ -1,2 +1,3 @@
 class Level < ApplicationRecord
+  has_many :games ,through: :game_levels
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :game_levels
+  resources :games
+  resources :levels
 end
