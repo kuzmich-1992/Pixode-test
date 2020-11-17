4b0d244029600b3746a0f8e41f514c9186a87b60
Dima <kuzmichman92@gmail.com>
2020-11-17 14:33:16 +0200

    change to includes

--- app/controllers/game_levels_controller.rb
+++ app/controllers/game_levels_controller.rb
@@ -1,7 +1,6 @@
 class GameLevelsController < ActionController::API
   def index 
-  	@game_level = GameLevel.all
-
+  	@game_level = GameLevel.includes(:game, :level)
   	render json: @game_level, fields: [:game_name, :game_level, :priority], adapter: :json
   end
 end
